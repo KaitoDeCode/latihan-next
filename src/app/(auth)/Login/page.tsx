bf3a16347241d47c1271e4b6212de6111b425585
KaitoDeCode <109795557+KaitoDeCode@users.noreply.github.com>
2023-12-24 08:36:04 -0800

    membuat login,register dan juga belajar link dan router

--- src/app/(auth)/Login/page.tsx
+++ src/app/(auth)/Login/page.tsx
@@ -1,8 +1,33 @@
+import Link from "next/link"
 
 type Props = {}
 
 export default function LoginPage({}: Props) {
   return (
-    <div>LoginPage</div>
+    <>
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                  
+                  <div>
+                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                      <input type="email" name="email" id="email" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                  </div>
+                  <div>
+                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                  </div>
+                  <div className="flex items-center justify-between">
+                      <a href="#" className="text-sm font-medium text-sky-600 hover:underline dark:text-sky-500">Forgot password?</a>
+                  </div>
+                  <button type="submit" className="w-full text-white bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">Sign in</button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <Link href="/register" className="font-medium text-sky-600 hover:underline dark:text-sky-500">Sign up</Link>
+                  </p>
+              </form>
+          </div>
+    </>
   )
 }
--- src/components/Fragments/Navbar.tsx
+++ src/components/Fragments/Navbar.tsx
@@ -1,19 +1,35 @@
+"use client"
+
 import Link from "next/link"
+import { usePathname,useRouter } from "next/navigation";
 
 const Navbar = () => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const disableNavbar = ["/login","/register"];
+
   return (
-    <nav className="px-14 py-5 sticky top-0 flex justify-between items-center bg-sky-700 text-white z-50">
+    <>
+        {!disableNavbar.includes(pathname) && ( 
+        <nav className="px-14 py-5 sticky top-0 flex justify-between items-center bg-sky-700 text-white z-50">
         <h1 className="font-bold">Navbar</h1>
-            <ul className="flex gap-6">
+            <ul className="flex gap-6 items-center">
                 {
                     navlist.map((data,index)=>(
                         <Link key={index} href={data.url}>
-                            <li>{data.text}</li>
+                            <li className={`${pathname === data.url && 'underline-offset-2 underline font-semibold text-sky-400'}`}>{data.text}</li>
                         </Link>
                     ))
                 }
+                <li>
+                    <button onClick={()=>router.push("/login")} className="bg-green-600 text-green-400 px-3 py-1 rounded">
+                        Login
+                    </button>
+                </li>
             </ul>
-    </nav>
+    </nav>) }
+    </>
   )
 }
 
